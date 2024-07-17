261c567ae08638bdef1119da22d2a9d74d16207b
DreaMeR <DreaMeR@DREAMER>
2024-07-17 11:33:08 +0600

    Добавьте файлы проекта.

---
+++ Multiples numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Multiples_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int minMultiple = 50;
+            int maxMultiple = 150;
+            int numberOfMultiples = 0;
+            int randomValue = random.Next(10, 26);
+
+            Console.WriteLine("Дано случайное число: " + randomValue);
+
+            for (int i = 0; i <= maxMultiple; i++)
+            {
+                i += randomValue;
+
+                if (i >= minMultiple)
+                {
+                    numberOfMultiples++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел от {minMultiple} до {maxMultiple}, которые кратыны {randomValue}: {numberOfMultiples} ");
+        }
+    }
+}
